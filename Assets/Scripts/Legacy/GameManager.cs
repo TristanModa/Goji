51c49ef6aef1c20d2c3cd5a4a47203d961658d48
Tristan Moda <tamoda@wpi.edu>
2025-01-27 17:57:19 -0500

    Created legacy scene
    Created legacy scene, scripts, assets, to preserve the initial build

---
+++ Assets/Scripts/Legacy/GameManager.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Goji.Legacy
+{
+	public class GameManagerLegacy : MonoBehaviour
+	{
+		[SerializeField]
+		SnakeControllerLegacy snake;
+
+		[SerializeField]
+		GameObject gameOverText;
+
+		void Start()
+		{
+			gameOverText.SetActive(false);
+		}
+
+		void Update()
+		{
+			if (snake.IsDead)
+				gameOverText.SetActive(true);
+
+			// Reload the scene if the player presses R
+			if (snake.IsDead && Input.GetKeyDown(KeyCode.R))
+				SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+		}
+	}
+}
